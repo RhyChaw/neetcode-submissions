d1e0025aaf4da9764c6a7341b22292391bbe4b5a
Rhythm <140189712+RhyChaw@users.noreply.github.com>
2026-05-25 20:19:37 +0300

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.py
@@ -0,0 +1,50 @@
+class Solution:
+    # def productExceptSelf(self, nums: List[int]) -> List[int]:
+    #     result = []
+    #     product = 1
+
+    #     for i in nums:
+    #         customNums = nums
+    #         customNums[i] = 1
+    #         result[i] = product(customNums)
+        
+    #     return result
+
+    # def product(self, nums: List[int]) -> int:
+    #     prod = 1
+    #     for num in nums:
+    #         prod *= num
+
+    #     return prod
+    
+    # O(n2)
+    # def productExceptSelf(self, nums: List[int]) -> List[int]:
+    #     result = []
+        
+    #     for i in range(len(nums)):        # use index, not value
+    #         customNums = nums.copy()       # copy so you don't modify original
+    #         customNums[i] = 1             # set current index to 1
+    #         result.append(self.product(customNums))  # append, not index
+        
+    #     return result
+
+    # def product(self, nums: List[int]) -> int:
+    #     prod = 1
+    #     for num in nums:
+    #         prod *= num
+    #     return prod
+
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+        
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        
+        return res
